19cc390d263a746390f2a64be13acd7d167171f9
Jefferson Ye <heroicevil7@gmail.com>
2021-01-14 14:42:00 -0600

    basic route

---
+++ pages/games/[id].js
@@ -0,0 +1,23 @@
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+
+export default function Game({ game }) {
+    const router = useRouter()
+    const { id } = router.query
+
+    return ( <>
+        <Head>
+            <title>{game.name}</title>
+        </Head>
+        <h1>{game.name} {game.id} {game.description}</h1>
+    </> )
+}
+
+export async function getServerSideProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`)
+    const data = await req.json()
+
+    return {
+        props: { game: data },
+    }
+}
